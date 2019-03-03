2e587222e1c241f34a3a162d07dd9e6cbc16661f
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 23:33:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
     @total *= 1 - @discount * 0.01
     discount_message = "After the discount, the total comes to $#{@total.to_i}."
     no_discount_message = "There is no discount to apply."
-    @discount > 0 ? discount_message : no_discount_message 
+    @discount > 0 ? discount_message : no_discount_message
   end
   def void_last_transaction
   end
