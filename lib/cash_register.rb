1127a554a562eb2fcb10a9e9ab8fba1f9078f446
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 23:20:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,8 +3,10 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
   def add_item(title, price, quantity=1)
+
   end
   def apply_discount
   end
