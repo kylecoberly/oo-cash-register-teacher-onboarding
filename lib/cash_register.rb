8dcd6d2ae0c800d6ab0b1e744efdb7ae45a99408
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 23:29:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     @total += price * quantity
   end
   def apply_discount
-    @total *= (1 - @discount * 0.01)
+    @total *= 1 - @discount * 0.01
     "After the discount, the total comes to $#{total.to_i}"
   end
   def void_last_transaction
